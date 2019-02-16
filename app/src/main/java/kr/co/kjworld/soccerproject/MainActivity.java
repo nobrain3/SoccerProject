443f102ae58e6c4989ab82a11de06d8e4fb5a1df
nobrain3 <todyl@naver.com>
2019-02-17 00:10:32 +0900

    로그인 화면 프레그먼트로 변경.

--- app/src/main/java/kr/co/kjworld/soccerproject/MainActivity.java
+++ app/src/main/java/kr/co/kjworld/soccerproject/MainActivity.java
@@ -1,7 +1,10 @@
 package kr.co.kjworld.soccerproject;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import kr.co.kjworld.soccerproject.login.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragment_login_container, LoginFragment.newInstance()).commit();
+
     }
 }
---
+++ app/src/main/java/kr/co/kjworld/soccerproject/login/LoginFragment.java
@@ -0,0 +1,28 @@
+package kr.co.kjworld.soccerproject.login;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kr.co.kjworld.soccerproject.R;
+
+public class LoginFragment extends Fragment {
+    
+    public static LoginFragment newInstance() {
+        
+        Bundle args = new Bundle();
+        
+        LoginFragment fragment = new LoginFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
