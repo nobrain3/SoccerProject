38cd587eb83cf79805cd7a762e0a59ceaacea78c
nobrain3 <todyl@naver.com>
2019-02-25 00:21:01 +0900

    카카오 로그인 연동. Hashcode 얻어오는 코드 추가. KakaoAdapter 구현  - 앱과 Kakao SDK와 연결해주는 기능  - SKD에 앱의 context와 (IApplicationConfig) 로그인을 위해 Session을 생성하기 위해 필요한 옵션을 설정하는 class(로그인을 위해 Session을 생성하기 위해 필요한 옵션)을 셋팅해준다.

--- app/src/main/java/kr/co/kjworld/soccerproject/MainActivity.java
+++ app/src/main/java/kr/co/kjworld/soccerproject/MainActivity.java
@@ -1,8 +1,16 @@
 package kr.co.kjworld.soccerproject;
 
 import android.app.FragmentTransaction;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import kr.co.kjworld.soccerproject.login.LoginFragment;
 
@@ -12,6 +20,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Add code to print out the key hash
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.e("kjkj MY KEY HASH:",
+                        Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+        } catch (NoSuchAlgorithmException e) {
+
+        }
 
         FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.fragment_login_container, LoginFragment.newInstance()).commit();
---
+++ app/src/main/java/kr/co/kjworld/soccerproject/login/kakao/KakaoSDKAdapter.java
@@ -0,0 +1,55 @@
+package kr.co.kjworld.soccerproject.login.kakao;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+
+import com.kakao.auth.ApprovalType;
+import com.kakao.auth.AuthType;
+import com.kakao.auth.IApplicationConfig;
+import com.kakao.auth.ISessionConfig;
+import com.kakao.auth.KakaoAdapter;
+
+public class KakaoSDKAdapter extends KakaoAdapter {
+    @Override
+    public IApplicationConfig getApplicationConfig() {
+        IApplicationConfig newIAppLicationConfig = new IApplicationConfig() {
+            @Override
+            public Context getApplicationContext() {
+                return getApplicationContext();
+            }
+        };
+        return newIAppLicationConfig;
+    }
+
+    @Override
+    public ISessionConfig getSessionConfig() {
+        ISessionConfig newSessionConfig = new ISessionConfig() {
+            @Override
+            public AuthType[] getAuthTypes() {
+                return new AuthType[] {AuthType.KAKAO_LOGIN_ALL};
+            }
+
+            @Override
+            public boolean isUsingWebviewTimer() {
+                return false;
+            }
+
+            @Override
+            public boolean isSecureMode() {
+                return false;
+            }
+
+            @Nullable
+            @Override
+            public ApprovalType getApprovalType() {
+                return ApprovalType.INDIVIDUAL;
+            }
+
+            @Override
+            public boolean isSaveFormData() {
+                return false;
+            }
+        };
+        return newSessionConfig;
+    }
+}
